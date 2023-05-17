183968b479a49c1ef316417e3142a86800fdae77
Soham Marik <soham.marik@automationedge.com>
2023-05-17 17:08:50 +0530

    ChatGPT clone

---
+++ first-api-app/chat.py
@@ -0,0 +1,25 @@
+import os
+
+import dotenv
+import openai
+
+dotenv.load_dotenv("../.env")
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+chat_messages = []
+
+while True:
+    user_message = input("You: ")
+
+    chat_messages.append({"role": "user", "content": user_message})
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=chat_messages,
+        temperature=0,
+    )
+
+    ai_response = response.choices[0].message.content
+    chat_messages.append({"role": "assistant", "content": ai_response})
+    print("AI:", ai_response)
