8562e831ca5510c875192f1622ad1ae2e83d0b33
Soham Marik <soham.marik@automationedge.com>
2023-05-18 18:32:48 +0530

    Wikipedia research

---
+++ langchain-intro/wikipedia_research.py
@@ -0,0 +1,19 @@
+import os
+
+import dotenv
+from langchain.agents import AgentType, initialize_agent, load_tools
+from langchain.llms import OpenAI
+
+dotenv.load_dotenv("../.env")
+
+llm = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"), temperature=0)
+
+tools = load_tools(["wikipedia", "llm-math"], llm=llm)
+
+agent = initialize_agent(
+    tools=tools, llm=llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+)
+
+prompt = input("The Wikipedia research task: ")
+
+agent.run(prompt)
